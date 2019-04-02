d50df5cd925774bdd70b18262bd515c0adeb90ca
Serhii Havrylenko <shavrylenko@livegenic.com>
2019-04-02 13:34:24 +0300

    commit

--- second.php
+++ second.php
@@ -49,3 +49,20 @@ switch ($a) {
 
     case 3: echo 'three'; break;
 }
+
+
+
+echo '<br/><br/><br/><br/><br/>' . 'Ебобо вариант switch с ;' . '<br/>';
+
+$topchick =  <<<ebobo
+switch (\$a) {
+    case 1; echo 'one'; break;
+
+    case 2; echo 'two'; break;
+
+    default; echo 'four'; break;
+
+    case 3; echo 'three'; break;
+}
+ebobo;
+echo nl2br($topchick);
