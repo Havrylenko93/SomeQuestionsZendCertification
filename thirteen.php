b53bbaac524d52d42f96f0dc1e3a5208b9e6de91
Serhii Havrylenko <shavrylenko@livegenic.com>
2019-05-02 12:09:48 +0300

    some info about bitwise

--- thirteen.php
+++ thirteen.php
@@ -1,35 +1,20 @@
 <?php
 
 $question = <<<QUE
-What is the result of the following bitwise operation in PHP?
 
-1 ^ 2    
+Your supervisor wants you to disallow PHP scripts to open remote HTTP and FTP resources using PHP's file functions. Which php.ini setting should you change accordingly?
 
-A. 1
-
-B. 3
-
-C. 2
-
-D. 4
-
-E: -1
 
 QUE;
 
 $answer = <<<ANSW
 __________________________________________________________________________________________________________________
 
-B
-1 = 01
-2 = 11
-    --
-    11 = 3
-    
+
 __________________________________________________________________________________________________________________
 
 ANSW;
 
-echo nl2br($question);
-echo nl2br(PHP_EOL);
-echo nl2br($answer);
+echo nl2br($question) . nl2br(PHP_EOL) . nl2br($answer);
+
+
