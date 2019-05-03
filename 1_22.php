048626ffd4a4f8b35b844d4baa38d58c8574cada
Serhii Havrylenko <shavrylenko@livegenic.com>
2019-05-03 16:49:26 +0300

    commit

--- 1_22.php
+++ 1_22.php
@@ -1,6 +1,7 @@
 <?php
 
-$question = <<<'QUE'
+namespace {
+    $question = <<<'QUE'
 Which of these elements can be encapsulated by namespaces and made accessible from the outside?
 
 A. Only classes
@@ -11,7 +12,7 @@ C. Classes, functions, constants and variables
 
 QUE;
 
-$answer = <<<ANSW
+    $answer = <<<ANSW
 __________________________________________________________________________________________________________________
 
 the correct answer is - B
@@ -19,4 +20,25 @@ ________________________________________________________________________________
 
 ANSW;
 
-echo nl2br($question) . nl2br(PHP_EOL) . nl2br($answer);
+    echo nl2br($question) . nl2br(PHP_EOL) . nl2br($answer);
+
+}
+
+namespace My\Functions {
+
+    function myFunction(string $param)
+    {
+        echo $param;
+    }
+}
+
+namespace MyConstants {
+    const MyConst = 'value';
+}
+
+namespace y {
+    use function My\Functions\myFunction;
+    use const MyConstants\MyConst;
+
+    myFunction(MyConst);
+}
